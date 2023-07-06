e3dd43e13df181a79283c307864a322108783843
Marli <marleysumarly@gmail.com>
2023-07-07 00:14:12 +0700

    Update

--- app.mjs
+++ app.mjs
@@ -1,5 +1,24 @@
 import http from "http";
+import {TodolistService} from "./todolist-service.mjs";
 
+const service = new TodolistService();
 const server = http.createServer((request, response) => {
-    response.write("Todolist API")
-})
+    response.setHeader("Content-Type", "application/json");
+
+    switch (request.method) {
+        case "GET":
+            service.getTodolist(request, response);
+            break;
+        case "POST":
+            service.createTodo(request, response);
+            break;
+        case "PUT":
+            service.updateTodo(request, response);
+            break;
+        case "DELETE":
+            service.deleteTodo(request, response);
+            break;
+    }
+});
+
+server.listen(3000);
